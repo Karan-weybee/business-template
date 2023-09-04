919c7f665ca529262c944d2ae1657ec5acd49663
Abe Lincoln <mrabe@git.training>
2023-09-04 17:21:25 +0530

    back to top added

--- backtotop.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('button1').addEventListener('click',function () {
-    document.querySelector('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-    return false;
-   })
