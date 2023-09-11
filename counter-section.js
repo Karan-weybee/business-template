751091ca8604ff94ed2a14d88945026d0e51c417
Abe Lincoln <mrabe@git.training>
2023-09-11 14:36:55 +0530

    class added

--- counter-section.js
+++ counter-section.js
@@ -1,14 +1,23 @@
-let counter =0;
-function counterValue(){
+let counter = 0;
+let a = Number(document.getElementById('count1').textContent);
+let b = Number(document.getElementById('count2').textContent);
+let c = Number(document.getElementById('count3').textContent);
+let d = Number(document.getElementById('count4').textContent);
+function counterValue() {
     counter++;
-    if(counter<501){
-document.getElementById('count1').innerHTML=counter
-document.getElementById('count2').innerHTML=counter
-document.getElementById('count3').innerHTML=counter
-document.getElementById('count4').innerHTML=counter
+    if (counter <= a) { document.getElementById('count1').innerHTML = counter }
+    if (counter <= b) {
+        document.getElementById('count2').innerHTML = counter
     }
-    else{
+    if (counter <= c) {
+        document.getElementById('count3').innerHTML = counter
+    }
+    if (counter <= d) {
+
+        document.getElementById('count4').innerHTML = counter
+    }
+    else {
         return;
     }
 }
-setInterval(counterValue,.00001);
+setInterval(counterValue, .00001);
